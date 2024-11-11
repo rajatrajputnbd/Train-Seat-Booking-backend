dc89045ad89c0dd820f2b933c19f2ad36630692f
codingpratice <codedotart.jsr@gmail.com>
2024-11-11 10:19:06 +0530

    Fix router handling and serverless deployment

--- db.js
+++ db.js
@@ -1,11 +1,30 @@
+// const mysql = require('mysql2');
+
+// // Create and export the MySQL connection
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Jsr@1234',
+//     database: 'train_booking'
+// });
+
+// db.connect(err => {
+//     if (err) throw err;
+//     console.log('Connected to MySQL database.');
+// });
+
+// module.exports = db;
+
+
+
 const mysql = require('mysql2');
 
-// Create and export the MySQL connection
+// Create and export the MySQL connection using environment variables
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Jsr@1234',
-    database: 'train_booking'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 });
 
 db.connect(err => {
