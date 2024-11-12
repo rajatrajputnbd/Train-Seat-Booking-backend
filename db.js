474923c80ad921842292e285bb298ad48a48ae7b
codingpratice <codedotart.jsr@gmail.com>
2024-11-12 11:24:37 +0530

    first commit bb vercel

--- db.js
+++ db.js
@@ -17,14 +17,21 @@
 
 
 
+
+// host: process.env.DB_HOST,
+// user: process.env.DB_USER,
+// password: process.env.DB_PASSWORD,
+// database: process.env.DB_NAME,
+
 const mysql = require('mysql2');
 
 // Create and export the MySQL connection using environment variables
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
 });
 
 db.connect(err => {
