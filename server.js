1ce5a21d20b83219234441d0488abce1955687f7
codingpratice <codedotart.jsr@gmail.com>
2024-11-12 11:51:48 +0530

    first commit bb vercel simple

--- server.js
+++ server.js
@@ -1,44 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const bodyParser = require('body-parser');
-const serverless = require('serverless-http');
-const routes = require('./routes'); // Import routes
-// const routes = require('./api/status'); // Import API routes from /api/status
-
-const app = express();
-
-// Middleware setup
-app.use(bodyParser.json());
-app.use(cors({ origin: 'http://localhost:3001' }));  // Enable CORS for React app
-
-// Use routes
-// app.use(routes);
-app.use('/api', routes); // Prefix routes with '/api' for cleaner paths
-
-// Start the server
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//     console.log(`Server running on port ${PORT}`);
-// });
-
-// Export the app wrapped in a serverless handler
-module.exports.handler = serverless(app);
-
-
-
-
-
 // const express = require('express');
-// const app = express();
-// const port = 3000;
+// const cors = require('cors');
+// require('dotenv').config();
+// const bodyParser = require('body-parser');
+// const serverless = require('serverless-http');
+// const routes = require('./routes'); // Import routes
+// // const routes = require('./api/status'); // Import API routes from /api/status
 
-// // Route for the root URL
-// app.get('/', (req, res) => {
-//     res.send('Hello, World!');
-// });
+// const app = express();
+
+// // Middleware setup
+// app.use(bodyParser.json());
+// app.use(cors({ origin: 'http://localhost:3001' }));  // Enable CORS for React app
+
+// // Use routes
+// // app.use(routes);
+// app.use('/api', routes); // Prefix routes with '/api' for cleaner paths
 
 // // Start the server
+// // const PORT = process.env.PORT || 3000;
+// // app.listen(PORT, () => {
+// //     console.log(`Server running on port ${PORT}`);
+// // });
+
+// // Export the app wrapped in a serverless handler
+// module.exports.handler = serverless(app);
+
+
+
+
+
+const express = require('express');
+const app = express();
+const port = 3000;
+const serverless = require('serverless-http');
+
+// Route for the root URL
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+// Start the server
 // app.listen(port, () => {
 //     console.log(`Server is running at http://localhost:${port}`);
 // });
+
+module.exports.handler = serverless(app);
